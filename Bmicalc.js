8dddc6bef24ca81b28deee5a9d668baf72e43c02
wedieu <wedieu.cole@hotmail.com>
2020-04-18 21:22:50 +0200

    bmicalc

---
+++ Bmicalc.js
@@ -0,0 +1,26 @@
+let height = 1.93;
+let weight = 82;
+let forumla = weight / (height * height);
+let result = 22.014013799028163;
+
+let underweight = 75;
+
+if (underweight >= 75 && underweight < 82) {
+  console.log("you are underweight");
+}
+
+
+let normalweight = 82;
+
+if (normalweight >= 82 && normalweight < 92) {
+  console.log("you have a normal weight");
+}
+
+let overweight = 92;
+
+if (overweight >= 92 && overweight < 100) {
+  console.log("you are overweight");
+}
+
+let obesity = 
+
--- Bmicalc.mjs
+++
@@ -1,4 +0,0 @@
-let height = 1.80; 
-let weight = 
-let forumla =  
-let result =
